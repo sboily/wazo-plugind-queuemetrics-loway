c4dd8dfa6021b9e627113b379ee3e12cb9444110
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-05-12 21:11:38 -0400

    add edit

--- wazo_plugind_admin_ui_queuemetrics_loway/plugin.py
+++ wazo_plugind_admin_ui_queuemetrics_loway/plugin.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 
+import json
+from urlparse import urlparse
+
 from flask_menu.classy import register_flaskview
 from flask_menu.classy import classy_menu_item
 
@@ -15,7 +18,7 @@ from wtforms.validators import InputRequired, Length, Regexp
 
 
 queuemetrics = create_blueprint('queuemetrics', __name__)
-
+configfile = '/etc/uniloader/uniloader.json'
 
 class Plugin(object):
 
@@ -32,7 +35,7 @@ class Plugin(object):
 class QueuemetricsForm(BaseForm):
     live_id = StringField('Live ID', [InputRequired(), Length(max=128)])
     password = StringField('Password', 
-                           [Length(max=80), Regexp(r'^[0-9]+$')],
+                           [Length(max=80)],
                            render_kw={'type': 'password',
                                       'data_toggle': 'password'})
     submit = SubmitField('Submit')
@@ -45,4 +48,33 @@ class QueuemetricsView(BaseView):
 
     @classy_menu_item('.queuemetrics', 'Queuemetrics', order=10, icon="bar-chart")
     def index(self):
-        return super(QueuemetricsView, self).index()
+        return super(QueuemetricsView, self).get(None)
+
+    def _map_resources_to_form(self, resource):
+        o = urlparse(resource[0].get('uri'))
+        live_id = o.path.split('/')[1]
+        data = {
+            'live_id': live_id,
+            'password': resource[0].get('pass')
+        }
+        form = self.form(data=data)
+        return form
+
+
+class QueuemetricsService(object):
+
+    def get(self, arg):
+        return self._read_config()
+
+    def update(self, resource):
+        config = self._read_config()
+        config[0]['uri'] = 'https://my.queuemetrics-live.com/{}'.format(resource.get('live_id'))
+        config[0]['pass'] = resource.get('password')
+
+        with open(configfile, 'w') as outfile:
+            json.dump(config, outfile, indent = 4)
+        return True
+
+    def _read_config(self):
+        with open(configfile) as json_data:
+            return json.load(json_data)
